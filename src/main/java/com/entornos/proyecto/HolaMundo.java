ae273436cad16388d0c5b2d03a8198b4100e6b0f
mperfecto <mperfectoarjona@gmail.com>
2021-05-26 17:13:06 +0200

    saluda con parametros

--- src/main/java/com/entornos/proyecto/HolaMundo.java
+++ src/main/java/com/entornos/proyecto/HolaMundo.java
@@ -1,20 +1,43 @@
 package com.entornos.proyecto;
- 
+
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
  
 @RestController
 public class HolaMundo {
 
-   @RequestMapping("/")
+   @GetMapping("/")
    public String saludar(){
-        String bienvenida = "Esta es tu primera web backend";
-       return bienvenida;
+       return "Esta es tu primera web backend";
    }
  
-   @RequestMapping("/despidete")
+   @GetMapping("/despidete")
    public String despidete(){
-        String despedida = "Adios amigo";
-       return despedida;
+        return "Adiós amigo";
    }
+
+   @GetMapping("/saludame/{nombre}")
+   public String saludame(@PathVariable String nombre){
+       return "Hola qué tal " + nombre;
+   }
+
+   @GetMapping("/saluda")
+   public String SaludaPorQuery(@RequestParam String nombre,@RequestParam String apellidos){
+       //return "Hola qué tal estás" + nombre + " " + apellidos;
+       Object params[] = {nombre, apellidos};
+       return MessageFormat.format("Qué tal estás {0} {1}", params);
+   }
+
+
 }
